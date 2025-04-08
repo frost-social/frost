239d7fde48efe944cd559f98b8056b1a38d4d0b2
marihachi <marihachi0620@gmail.com>
2025-04-08 20:11:07 +0000

    refactor

--- snow-spec/src/generate.ts
+++ snow-spec/src/generate.ts
@@ -1,5 +1,22 @@
-import * as M from "./middleTree";
+import util from "node:util";
+import * as S from "./snowTree";
+import * as M from "./openapiTree";
 
-export function generate(file: M.MFile): string {
-  throw new Error("not implemented yet");
+export function generate(file: S.SFile): string {
+  const outFile: M.MFile = {
+    openapi: "3.1.0",
+    paths: {},
+  };
+
+  for (const fileMember of file.children) {
+    if (fileMember.kind == "route") {
+      if (outFile.paths[fileMember.path] == null) {
+        outFile.paths[fileMember.path] = {};
+      }
+    }
+  }
+
+  console.log(util.inspect(outFile, { depth: 30 }));
+
+  return JSON.stringify(outFile);
 }
--- snow-spec/src/main.ts
+++ snow-spec/src/main.ts
@@ -1,18 +1,14 @@
 import fs from "node:fs/promises";
 import Path from "node:path";
 import { parse } from "./parse";
-import util from "node:util";
-import { convertTree } from "./convertTree";
-//import { generate } from "./generate";
+import { generate } from "./generate";
 
 async function main() {
   const input = await fs.readFile(Path.resolve("debug.txt"), { encoding: "utf-8" });
   const snowTree = parse(input);
   //console.log(util.inspect(snowTree, { depth: 30 }));
-  const middleTree = convertTree(snowTree);
-  console.log(util.inspect(middleTree, { depth: 30 }));
-  //const openApiText = generate(middleTree);
-  //console.log(openApiText);
+  const json = generate(snowTree);
+  //console.log(json);
 }
 main()
 .catch(err => { console.error(err); });
