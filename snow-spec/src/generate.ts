897676d2cdf5d278ebdc8b096a11cf2169ef83af
marihachi <marihachi0620@gmail.com>
2025-04-09 19:00:44 +0000

    declare OpenAPI Specification Schema

--- snow-spec/src/generate.ts
+++ snow-spec/src/generate.ts
@@ -1,22 +1,29 @@
 import util from "node:util";
 import * as S from "./snowTree";
-import * as M from "./openapiTree";
+import * as O from "./openapiSchema";
 
 export function generate(file: S.SFile): string {
-  const outFile: M.MFile = {
+  const outFile: O.OpenAPI = {
     openapi: "3.1.0",
+    info: {
+      title: "",
+      version: ""
+    },
     paths: {},
   };
 
   for (const fileMember of file.children) {
     if (fileMember.kind == "route") {
-      if (outFile.paths[fileMember.path] == null) {
-        outFile.paths[fileMember.path] = {};
+      if (outFile.paths![fileMember.path] == null) {
+        outFile.paths![fileMember.path] = {};
       }
     }
   }
 
   console.log(util.inspect(outFile, { depth: 30 }));
 
-  return JSON.stringify(outFile);
+  const json = JSON.stringify(outFile);
+  //console.log(json);
+
+  return json;
 }
--- snow-spec/src/openapiTree.ts
+++
@@ -1,9 +0,0 @@
-export type MNode = MFile;
-
-export type MFile = {
-  openapi: string,
-  paths: Record<string, MPath>,
-};
-
-export type MPath = {
-};
