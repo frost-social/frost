edfb7cfad68d4f7e54d8d8785d493cdc81806a16
marihachi <marihachi0620@gmail.com>
2025-04-10 21:05:40 +0000

    update project

--- snow-spec/src/syntaxNode.ts
+++ snow-spec/src/syntaxNode.ts
@@ -15,7 +15,7 @@ export interface AttrNode {
   kind: 'attr';
   key: string;
   value?: ValueNode;
-};
+}
 
 export interface RouteNode {
   kind: 'route';
@@ -23,62 +23,62 @@ export interface RouteNode {
   path: string;
   children: RouteMemberNode[];
   attrs: AttrNode[];
-};
+}
 
 export interface RequestNode {
   kind: "request";
   blocks: ComponentBlockNode[];
   attrs: AttrNode[];
-};
+}
 
 export interface ResponseNode {
   kind: "response";
   statusCode: string;
   blocks: ComponentBlockNode[];
   attrs: AttrNode[];
-};
+}
 
 export interface ComponentBlockNode {
   kind: "componentBlock";
   blockKind: string;
   component: ComponentNode;
   attrs: AttrNode[];
-};
+}
 
 export interface ComponentRefNode {
   kind: "componentRef";
   name: string;
-};
+}
 
 export interface ObjectNode {
   kind: "object";
   children: ObjectFieldNode[];
-};
+}
 
 export interface ObjectFieldNode {
   kind: "objectField";
   name: string;
   value: ComponentNode;
   attrs: AttrNode[];
-};
+}
 
 export interface NumberValueNode {
   kind: "numberValue";
   value: string;
-};
+}
 
 export interface BoolValueNode {
   kind: "boolValue";
   value: string;
-};
+}
 
 export interface StringValueNode {
   kind: "stringValue";
   value: string;
-};
+}
 
 export interface ComponentDeclNode {
   kind: "componentDecl";
   name: string;
   component?: ComponentNode;
-};
+}
