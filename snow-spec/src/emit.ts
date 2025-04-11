18399a7a89b5c4cae84a9c1b3eee3de2f53f8643
marihachi <marihachi0620@gmail.com>
2025-04-11 10:41:35 +0000

    emit

--- snow-spec/src/emit.ts
+++ snow-spec/src/emit.ts
@@ -8,18 +8,20 @@ export function emit(file: Symbols.FileSymbol): string {
       title: "",
       version: ""
     },
-    paths: {},
-    components: {
-      requestBodies: {},
-    },
   };
 
   for (const fileMember of file.children) {
     if (fileMember.kind == "route") {
-      emitRoute(fileMember, outFile);
+      if (outFile.paths == null) {
+        outFile.paths = {};
+      }
+      emitRoute(fileMember, outFile.paths);
     }
     if (fileMember.kind == "componentBase") {
-      emitComponentBase(fileMember, outFile);
+      if (outFile.components == null) {
+        outFile.components = {};
+      }
+      emitComponentBase(fileMember, outFile.components);
     }
   }
 
@@ -30,18 +32,52 @@ export function emit(file: Symbols.FileSymbol): string {
   return json;
 }
 
-function emitRoute(symbol: Symbols.RouteSymbol, root: Nodes.OpenAPI): void {
+function emitRoute(symbol: Symbols.RouteSymbol, target: Record<string, Nodes.PathItemObject>): void {
   for (const endpoint of symbol.children) {
-    if (root.paths![endpoint.syntaxNode.path] == null) {
-      root.paths![endpoint.syntaxNode.path] = {};
+    if (target[endpoint.syntaxNode.path] == null) {
+      target[endpoint.syntaxNode.path] = { };
+    }
+    if (endpoint.syntaxNode.method == "GET") {
+      if (target[endpoint.syntaxNode.path]!.get == null) {
+        target[endpoint.syntaxNode.path]!.get = {};
+      }
+    }
+    if (endpoint.syntaxNode.method == "POST") {
+      if (target[endpoint.syntaxNode.path]!.post == null) {
+        target[endpoint.syntaxNode.path]!.post = {};
+      }
+    }
+    if (endpoint.syntaxNode.method == "PUT") {
+      if (target[endpoint.syntaxNode.path]!.put == null) {
+        target[endpoint.syntaxNode.path]!.put = {};
+      }
+    }
+    if (endpoint.syntaxNode.method == "PATCH") {
+      if (target[endpoint.syntaxNode.path]!.patch == null) {
+        target[endpoint.syntaxNode.path]!.patch = {};
+      }
+    }
+    if (endpoint.syntaxNode.method == "DELETE") {
+      if (target[endpoint.syntaxNode.path]!.delete == null) {
+        target[endpoint.syntaxNode.path]!.delete = {};
+      }
     }
   }
 }
 
-function emitComponentBase(symbol: Symbols.ComponentBaseSymbol, root: Nodes.OpenAPI): void {
+function emitComponentBase(symbol: Symbols.ComponentBaseSymbol, target: Nodes.ComponentsObject): void {
   for (const component of symbol.children) {
+    if (component.kind == "parameterComponent") {
+      if (target.parameters == null) {
+        target.parameters = {};
+      }
+      target.parameters[symbol.syntaxNode.name] = {};
+    }
     if (component.kind == "requestBodyComponent") {
-      root.components!.requestBodies![symbol.syntaxNode.name] = {};
+      if (target.requestBodies == null) {
+        target.requestBodies = {};
+      }
+      target.requestBodies[symbol.syntaxNode.name] = {};
     }
   }
 }
