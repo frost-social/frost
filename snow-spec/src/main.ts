ed3fd272ffd7738278e30108f3941aa91f67ba6b
marihachi <marihachi0620@gmail.com>
2025-04-26 07:38:59 +0000

    fix snow-spec

--- snow-spec/src/main.ts
+++ snow-spec/src/main.ts
@@ -1,15 +1,15 @@
 import fs from "node:fs/promises";
 import Path from "node:path";
-import { resolve } from "./resolve";
 import { emit } from "./emit";
 import { parse } from "./parse";
+import { resolve } from "./resolve";
 
 async function main() {
   const input = await fs.readFile(Path.resolve("debug.txt"), { encoding: "utf-8" });
   const syntaxNode = parse(input);
   //console.log(util.inspect(syntaxNode, { depth: 30 }));
   resolve(syntaxNode);
-  const json = emit(syntaxNode);
+  //const json = emit();
 }
 main()
 .catch(err => { console.error(err); });
