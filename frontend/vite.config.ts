42d44eb4b308396ebe67d09b925f1e9b30df8691
しみ <37952374+Simirall@users.noreply.github.com>
2025-04-29 21:21:27 +0900

    Frontend Update (#165)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,24 @@
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import reactSwc from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tsconfigPaths()],
+// https://vite.dev/config/
+export default defineConfig(({ command }) => ({
+  plugins: [
+    tsconfigPaths(),
+    TanStackRouterVite(),
+    command === "serve"
+      ? reactSwc() // 開発時はSWCプラグインを使用
+      : react({
+          // ビルド時は標準のReactプラグイン+React Compilerを使用
+          babel: {
+            plugins: ["babel-plugin-react-compiler"],
+          },
+        }),
+  ],
   server: {
-    host: "0.0.0.0",
+    host: true,
   },
-});
+}));
