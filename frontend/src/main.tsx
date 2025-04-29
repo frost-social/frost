e16af4a5874bb843f234fdd23aa143e2672b9db1
Simirall <37952374+Simirall@users.noreply.github.com>
2025-04-29 12:16:31 +0000

    use tsq instead of swr

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "@mantine/core/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routeTree } from "./routeTree.gen";
 import { themeConfig } from "./style/themeConfig.tsx";
 
-const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: "intent",
+  context: {
+    queryClient: queryClient,
+  },
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -18,7 +27,9 @@ declare module "@tanstack/react-router" {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MantineProvider theme={themeConfig} defaultColorScheme="auto">
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </MantineProvider>
   </React.StrictMode>,
 );
