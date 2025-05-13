a7d38deb0e37b7566d35131004ad068c797deb1d
marihachi <marihachi0620@gmail.com>
2025-05-13 12:49:07 +0000

    wip

--- hono-expt/src/main.ts
+++ hono-expt/src/main.ts
@@ -5,7 +5,50 @@ import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi';
 // - https://hono.dev/docs/getting-started/nodejs
 // - https://hono.dev/examples/zod-openapi
 
+const echoGetInput = z.object({
+  message: z.string()
+    .openapi({
+      param: {
+        name: "message",
+        in: "query",
+      },
+    }),
+});
+
+const echoGetOutput = z.object({
+  message: z.string()
+    .openapi({
+      example: ""
+    }),
+}).openapi('Echo');
+
+const echoGet = createRoute({
+  method: "get",
+  path: "/api/v1/echo",
+  request: {
+    query: echoGetInput,
+  },
+  responses: {
+    200: {
+      content: {
+        "application/json": {
+          schema: echoGetOutput,
+        }
+      },
+      description: "OK",
+    },
+  }
+});
+
 const app = new OpenAPIHono();
+
+app.openapi(echoGet, (c) => {
+  const message = c.req.query("message");
+  c.status(200);
+  return c.json({
+    message: message,
+  });
+});
 
 const server = serve(app, (x) => {
   console.log(`listing started on http://localhost:${x.port}/`);
