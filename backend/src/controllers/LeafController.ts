eabac3406f42ff1566c0897df914293c58af6082
marihachi <marihachi0620@gmail.com>
2025-05-21 14:35:28 +0000

    leaf controller

--- backend/src/controllers/LeafController.ts
+++ backend/src/controllers/LeafController.ts
@@ -7,85 +7,117 @@ import {
   defineApiRoute,
   validateApiData,
 } from "../core/restApi.js";
+import { createLeaf, deleteLeaf, getLeaf } from "../services/LeafService.js";
+import type { UserObject } from "../services/UserService.js";
 
 export const apiBasePath = "/api/v1";
 
+export const leafSchema = z.object({
+  leafId: z.string(),
+});
+export type LeafSchema = z.infer<typeof leafSchema>;
+
+// CreateLeaf
+export const createLeafRoute = defineApiRoute({
+  method: "post",
+  path: `${apiBasePath}/leaf/createLeaf`,
+  inputSchema: z.object({
+    content: z.string(),
+  }),
+  outputSchema: leafSchema,
+});
+export type CreateLeafInputSchema = z.infer<typeof createLeafRoute.inputSchema>;
+export type CreateLeafOutputSchema = z.infer<typeof createLeafRoute.outputSchema>;
+
+// DeleteLeaf
+export const deleteLeafRoute = defineApiRoute({
+  method: "post",
+  path: `${apiBasePath}/leaf/deleteLeaf`,
+  inputSchema: z.object({
+    leafId: z.string(),
+  }),
+  outputSchema: z.object({
+  }),
+});
+export type DeleteLeafInputSchema = z.infer<typeof deleteLeafRoute.inputSchema>;
+export type DeleteLeafOutputSchema = z.infer<typeof deleteLeafRoute.outputSchema>;
+
+// GetLeaf
+export const getLeafRoute = defineApiRoute({
+  method: "get",
+  path: `${apiBasePath}/leaf/getLeaf`,
+  inputSchema: z.object({
+    leafId: z.string(),
+  }),
+  outputSchema: leafSchema,
+});
+export type GetLeafInputSchema = z.infer<typeof getLeafRoute.inputSchema>;
+export type GetLeafOutputSchema = z.infer<typeof getLeafRoute.outputSchema>;
+
+// SearchLeafs
+export const searchLeafsRoute = defineApiRoute({
+  method: "get",
+  path: `${apiBasePath}/leaf/searchLeafs`,
+  inputSchema: z.object({
+    userId: z.string().optional(),
+  }),
+  outputSchema: z.object({
+    items: leafSchema.array(),
+  }),
+});
+export type SearchLeafsInputSchema = z.infer<typeof searchLeafsRoute.inputSchema>;
+export type SearchLeafsOutputSchema = z.infer<typeof searchLeafsRoute.outputSchema>;
+
 export function leafController(router: Router, db: DB) {
+  router.post(
+    "/leaf/createLeaf",
+    tokenAuth(),
+    checkScope("leaf.write"),
+    async (req, res) => {
+      const ctx = await createRequestContext(req.user as UserObject, db);
+      const params = validateApiData(createLeafRoute.inputSchema, req.body);
+      const result = await createLeaf(ctx, params);
+      validateApiData(createLeafRoute.outputSchema, result);
+      res.json(result);
+    },
+  );
 
+  router.post(
+    "/leaf/deleteLeaf",
+    tokenAuth(),
+    checkScope("leaf.delete"),
+    async (req, res) => {
+      const ctx = await createRequestContext(req.user as UserObject, db);
+      const params = validateApiData(deleteLeafRoute.inputSchema, req.body);
+      const result = await deleteLeaf(ctx, params);
+      validateApiData(deleteLeafRoute.outputSchema, result);
+      res.json(result);
+    },
+  );
+
+  router.get(
+    "/leaf/getLeaf",
+    tokenAuth(),
+    checkScope("leaf.read"),
+    async (req, res) => {
+      const ctx = await createRequestContext(req.user as UserObject, db);
+      const params = validateApiData(getLeafRoute.inputSchema, req.query);
+      const result = await getLeaf(ctx, params);
+      validateApiData(getLeafRoute.outputSchema, result);
+      res.json(result);
+    },
+  );
+
+  router.get(
+    "/leaf/searchLeafs",
+    tokenAuth(),
+    checkScope("leaf.read"),
+    async (req, res) => {
+      throw new Error("not implemented");
+      //const ctx = await createRequestContext(req.user as UserObject, db);
+      //const params = validateApiData(searchLeafRoute.inputSchema, req.query);
+      //const result = await searchLeaf(ctx, params);
+      //validateApiData(searchLeafRoute.outputSchema, result);
+    },
+  );
 }
-
-// registerRoute(router, db, {
-//   method: "POST",
-//   path: "/leaf/createLeaf",
-//   scope: "leaf.write",
-//   async requestHandler(
-//     ctx,
-//   ): Promise<Endpoints["/api/v1/leaf/createLeaf"]["result"]> {
-//     const params: Endpoints["/api/v1/leaf/createLeaf"]["body"] =
-//       ctx.validateParams(
-//         z.object({
-//           content: z.string().min(1),
-//         }),
-//       );
-//     const result = await LeafService.createLeaf(
-//       params,
-//       { userId: ctx.getUser().userId },
-//       ctx.db,
-//     );
-//     return result;
-//   },
-// });
-
-// registerRoute(router, db, {
-//   method: "POST",
-//   path: "/leaf/deleteLeaf",
-//   scope: "leaf.delete",
-//   async requestHandler(
-//     ctx,
-//   ): Promise<Endpoints["/api/v1/leaf/deleteLeaf"]["result"]> {
-//     const params: Endpoints["/api/v1/leaf/deleteLeaf"]["body"] =
-//       ctx.validateParams(
-//         z.object({
-//           leafId: zUuid,
-//         }),
-//       );
-//     await LeafService.deleteLeaf(
-//       params,
-//       { userId: ctx.getUser().userId },
-//       ctx.db,
-//     );
-//   },
-// });
-
-// registerRoute(router, db, {
-//   method: "GET",
-//   path: "/leaf/getLeaf",
-//   scope: "leaf.read",
-//   async requestHandler(
-//     ctx,
-//   ): Promise<Endpoints["/api/v1/leaf/getLeaf"]["result"]> {
-//     const params: Endpoints["/api/v1/leaf/getLeaf"]["query"] =
-//       ctx.validateParams(
-//         z.object({
-//           leafId: zUuid,
-//         }),
-//       );
-//     const result = await LeafService.getLeaf(
-//       params,
-//       { userId: ctx.getUser().userId },
-//       ctx.db,
-//     );
-//     return result;
-//   },
-// });
-
-// registerRoute(router, db, {
-//   method: "GET",
-//   path: "/leaf/searchLeafs",
-//   scope: "leaf.read",
-//   async requestHandler(
-//     ctx,
-//   ): Promise<Endpoints["/api/v1/leaf/searchLeafs"]["result"]> {
-//     throw new Error("not implemented");
-//   },
-// });
